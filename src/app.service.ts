84c745a6354a0342146d09d23579b44e39e4754f
Chi Tran <chi@bespokify.com>
2023-09-06 14:02:40 +0700

    fix: upgrade queue util

--- src/app.service.ts
+++ src/app.service.ts
@@ -38,15 +38,20 @@ export class AppService implements OnModuleInit {
     const _ = await this.api.rpc.chain.subscribeFinalizedHeads(
       async (lastHeader) => {
         this.logger.debug(
-          `[${this.queueService.getProducerQueueType(
-            this.configService.get('QUEUE_NAME'),
-          ).toLocaleUpperCase()}] ${this.chainInfo.chainId}_${
-            this.chainInfo.version
-          } ---> Processing block ${lastHeader.hash.toString().substring(55)}`,
+          `[${this.queueService
+            .getProducerQueueType(this.configService.get('QUEUE_NAME'))
+            .toLocaleUpperCase()} | ${this.configService.get('QUEUE_NAME')}] ${
+            this.chainInfo.chainId
+          }_${this.chainInfo.version} ---> Processing block ${lastHeader.hash
+            .toString()
+            .substring(55)}`,
         );
 
         const data = await this.parseBlock(lastHeader.hash.toString());
-        await this.queueService.send(this.configService.get('QUEUE_NAME'), { id: data.id, body: data });
+        await this.queueService.send(this.configService.get('QUEUE_NAME'), {
+          id: data.id,
+          body: data,
+        });
       },
     );
   }
